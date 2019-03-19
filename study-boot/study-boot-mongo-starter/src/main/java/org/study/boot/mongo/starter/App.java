a5186e54f5c052940b36ac6c426a8fdd2b2078df
caozhenwei <Zhenwei.Cao@b-and-qchina.com>
2019-03-19 11:43:48 +0800

    update

---
+++ study-boot/study-boot-mongo-starter/src/main/java/org/study/boot/mongo/starter/App.java
@@ -0,0 +1,13 @@
+package org.study.boot.mongo.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
